25ab8a8e759c0fbfb0afe79103bfb223e66041e5
mcasey21 <157999240+mcasey21@users.noreply.github.com>
2024-01-28 23:14:46 +0000

    Add files via upload

---
+++ euclidian_website.py
@@ -0,0 +1,44 @@
+import streamlit as st
+
+"""
+# GCD CALCULATOR
+formula: a = q(b) + r
+"""
+
+num1 = st.number_input("Enter num1")
+num2 = st.number_input("Enter num2")
+
+if num1 == num2:
+    st.write(F"# GCD: {num1}")
+
+elif num1 and num2:
+    r_list = []
+
+    if num1 > num2:
+        a = num1
+        b = num2
+    else:
+        a = num2
+        b = num1
+
+    q = a//b
+    r = a - (q*b)
+
+    r_list.append(r)
+
+    st.write(F"{a} = {q}({b}) + {r}")
+
+    while r != 0:
+        if r != 0:
+            a = b
+            b = r
+        
+        q = a//b
+        r = a - (q*b)
+
+        r_list.append(r)
+
+        st.write(F"{a} = {q}({b}) + {r}")
+
+    if r == 0:
+        st.write(F"# GCD: {r_list[-2]}")
